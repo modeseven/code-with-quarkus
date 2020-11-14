d8b800251725db71454c1e8a52111b1bb2a71206
mbegley <mikeabegley@gmail.com>
2020-11-14 09:01:37 -0500

    health check and refactor start

--- src/main/java/org/begley/domain/AppointmentRequest.java
+++ src/main/java/org/begley/domain/AppointmentRequest.java
@@ -10,57 +10,9 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 @Entity(name="appt_rqst")
 public class AppointmentRequest extends PanacheEntity {
 
-    private String status;
-    private String apptType;
-    private String subjectId;
-    private String firstName;
-    private String lastName;
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getApptType() {
-        return apptType;
-    }
-
-    public void setApptType(String apptType) {
-        this.apptType = apptType;
-    }
-
-    public String getSubjectId() {
-        return subjectId;
-    }
-
-    public void setSubjectId(String subjectId) {
-        this.subjectId = subjectId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "AppointmentRequest [apptType=" + apptType + ", firstName=" + firstName + ", lastName=" + lastName
-                + ", status=" + status + ", subjectId=" + subjectId + "]";  
-    }
-
-    
+    public String status;
+    public String apptType;
+    public String subjectId;
+    public String firstName;
+    public String lastName;    
 }
--- src/main/java/org/begley/resources/ApptRequestResource.java
+++ src/main/java/org/begley/resources/ApptRequestResource.java
@@ -2,44 +2,72 @@ package org.begley.resources;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.begley.domain.AppointmentRequest;
+import org.begley.domain.BookingRequest;
 import org.begley.services.NatsBroker;
 
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
-@Path("api/appointmentrequest")
+import java.util.List;
+import java.util.Optional;
+
+@Path("api/apptrqst")
 public class ApptRequestResource {
 
     @Inject
     NatsBroker nats;
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<AppointmentRequest> apptRqst() {
+        return AppointmentRequest.listAll();
+    }
+
     @Transactional
     @GET
     @Path("/{id}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Response find(@PathParam("id") Long id) {
-
-        AppointmentRequest ar = new AppointmentRequest();
-        ar.setApptType("apptType");
-        ar.setFirstName("firstName");
-        ar.setLastName("lastName");
-        ar.setSubjectId("subjectId");
-        ar.setStatus("status");
-        ar.persistAndFlush();
-
-        nats.publish("subject", "hello nats");
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response find(@PathParam("id") Long id) {       
 
         return AppointmentRequest.findByIdOptional(id)
-            .map(u -> Response.ok( u.toString()))
+            .map(u -> Response.ok( u))
             .orElseGet(() -> Response.status(NOT_FOUND))
             .build();
 
+    }
+
+    @Transactional
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response newApptRqst(AppointmentRequest apptrqst) {
+        apptrqst.id = null;
+        apptrqst.persist();
+        return Response.status(Status.CREATED).entity(apptrqst).build();
+    }
+
+    @Transactional
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response bookingRequest(BookingRequest bookrqst) {
+        return AppointmentRequest.findByIdOptional(bookrqst.id)
+            .map(u -> {
+                nats.publish("bookingRqst", bookrqst.toString());
+                return Response.ok( u);
+            })
+            .orElseGet(() -> Response.status(NOT_FOUND))
+            .build();
     }
 }
